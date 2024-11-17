213eef0772bdfc7f4f77c7eafefd372270d32fdb
Claudalyn <e2240360@cmaisonneuve.qc.ca>
2024-11-17 14:56:23 -0500

    Modification de base

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import Connexion from './composants/Connexion/Connexion.jsx'
-import {Route, Router, Routes} from "react-router-dom";
+import Success from './composants/Connexion/Success.jsx'
+import {Route, Routes} from "react-router-dom";
 
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Connexion />} />
         <Route path="/success" element={<Success />} />
       </Routes>
-    </Router>
   );
 }
 
