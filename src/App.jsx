3408b01b60ce7e30d721384f6000cd1a491a73ea
NickEtudiant <e2266845@cmaisonneuve.qc.ca>
2024-11-27 10:10:42 -0500

    Firebase pour Inscription et Connexion fonctionnel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import {Route, BrowserRouter, Routes} from "react-router-dom";
-import {SignUp} from "./composants/SignUp/SignUp.jsx";
+import { Route, BrowserRouter, Routes} from "react-router-dom";
+import { SignUp } from "./composants/SignUp/SignUp.jsx";
+import { Login } from "./composants/Login/Login.jsx";
 
 function App() {
   return (
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignUp />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/inscription" element={<SignUp />} />
+          <Route path="/connexion" element={<Login />} />
+        </Routes>
       </BrowserRouter>
   );
 }
